899a318ab72117da3050904a3e7f57b9e485b9d9
mhafidk <mhafidk@gmail.com>
2022-07-16 10:48:32 +0700

    add footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,3 @@
+<footer class="pt-4 text-sm text-center">
+	Made with &#128154; by <span class="font-medium tracking-tighter">MHK</span>
+</footer>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,11 @@
 <script>
 	import '../app.css';
 	import Navbar from '$lib/components/navbar.svelte';
+	import Footer from '$lib/components/footer.svelte';
 </script>
 
 <main class="max-w-screen-2xl mx-auto p-4">
 	<Navbar />
 	<slot />
+	<Footer />
 </main>
