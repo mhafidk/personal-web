80da1314a1769bc3dc3182dd7e7bf8e37820d393
mhafidk <mhafidk@gmail.com>
2022-07-18 20:04:08 +0700

    update sitemap

--- src/routes/sitemap.xml.js
+++ src/routes/sitemap.xml.js
@@ -1,10 +1,12 @@
 import { client } from '$lib/graphql-client';
 import { gql } from 'graphql-request';
-
-const website = 'https://www.mhafidk.com';
+import { get as metadata } from './site-metadata.json.js';
 
 export const get = async () => {
-	const query = gql`
+	const {
+		body: { siteUrl }
+	} = await metadata();
+	const queryPosts = gql`
 		query Posts {
 			posts {
 				title
@@ -12,9 +14,23 @@ export const get = async () => {
 			}
 		}
 	`;
-	const { posts } = await client.request(query);
-	const pages = [`about`];
-	const body = sitemap(posts, pages);
+	const queryProjects = gql`
+		query Projects {
+			projects {
+				name
+				slug
+			}
+		}
+	`;
+	const [postsRes, projectsRes] = await Promise.all([
+		client.request(queryPosts),
+		client.request(queryProjects)
+	]);
+	const { posts } = postsRes;
+	const { projects } = projectsRes;
+
+	const pages = [`projects`, `posts`, `about`];
+	const body = sitemap(posts, projects, pages, siteUrl);
 
 	const headers = {
 		'Cache-Control': 'max-age=0, s-maxage=3600',
@@ -26,7 +42,7 @@ export const get = async () => {
 	};
 };
 
-const sitemap = (posts, pages) => `<?xml version="1.0" encoding="UTF-8" ?>
+const sitemap = (posts, projects, pages, siteUrl) => `<?xml version="1.0" encoding="UTF-8" ?>
 <urlset
   xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
   xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
@@ -36,7 +52,7 @@ const sitemap = (posts, pages) => `<?xml version="1.0" encoding="UTF-8" ?>
   xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
 >
   <url>
-    <loc>${website}</loc>
+    <loc>${siteUrl}</loc>
     <changefreq>daily</changefreq>
     <priority>0.7</priority>
   </url>
@@ -44,7 +60,7 @@ const sitemap = (posts, pages) => `<?xml version="1.0" encoding="UTF-8" ?>
 		.map(
 			(page) => `
   <url>
-    <loc>${website}/${page}</loc>
+    <loc>${siteUrl}/${page}</loc>
     <changefreq>daily</changefreq>
     <priority>0.7</priority>
   </url>
@@ -55,7 +71,18 @@ const sitemap = (posts, pages) => `<?xml version="1.0" encoding="UTF-8" ?>
 		.map(
 			(post) => `
   <url>
-    <loc>${website}/posts/${post.slug}</loc>
+    <loc>${siteUrl}/posts/${post.slug}</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.7</priority>
+  </url>
+  `
+		)
+		.join('')}
+  ${projects
+		.map(
+			(project) => `
+  <url>
+    <loc>${siteUrl}/projects/${project.slug}</loc>
     <changefreq>daily</changefreq>
     <priority>0.7</priority>
   </url>
